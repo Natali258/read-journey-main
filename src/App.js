cf67dd702b8a59286bf8c2e2595406bcbfdc98ca
Natali258 <natalya.holyavko@gmail.com>
2025-12-15 22:02:23 +0200

    add pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import { Route, Routes } from "react-router-dom";
 import { Register } from "./pages/Register";
 import { Login } from "./pages/Login";
+import { Recommended } from "./pages/Recommended";
+import { Library } from "./pages/Library";
+import { MyBook } from "./pages/MyBook";
 
 
 function App() {
@@ -8,9 +11,12 @@ function App() {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<Register/>} />
+        <Route path="/" element={<Recommended/>} />
         <Route path="/register" element={<Register/>}></Route>
         <Route path="/login" element={<Login/>}></Route>
+        <Route path="/recommended" element={<Recommended/>}></Route>
+        <Route path="/library" element={<Library/>}></Route>
+        <Route path="/reading" element={<MyBook/>}></Route>
       </Routes>
     </div>
   );
